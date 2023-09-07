a612b107dae2e97a469ddfe796b557bd6506a0e6
Jorge Vera <112434926+jorgeaveraf@users.noreply.github.com>
2023-09-07 12:28:39 -0600

    Header closes (#2)
    * Primer cambio en el header
    
    * create services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import	{  HttpClientModule	}	from	'@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkExperienceComponent
+    WorkExperienceComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../Services/header.service'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  name: string = "Jorge";
+  title: string = "Frontend Developer";
+  objetive: string = "I want to be a great developer";
+  photo: string = "aquí va una imagen";
+  email: string = "jorgeaveraf@gmail.com";
+  phone: string = "272 238 7763";
+  location: string = "Orizaba, Veracruz, México";
+  social: string = "@DER";
+
+  constructor(private headerService: HeaderService) { }
+
+  ngOnInit(): void {
+    this.headerService.getHeader().subscribe((data: any) => {
+      console.log(data);
+
+      this.name = data.name;
+      this.title = data.title;
+      this.objetive = data.objective;
+      this.photo = data.photo;
+      this.email = data.email;
+      this.phone = data.phone;
+      this.location = data.location;
+      this.social = data.social;
+    }
+    );
+  }
+
+
+
+
+
 
 }
