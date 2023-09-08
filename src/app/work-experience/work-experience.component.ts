c5063190bf7072f840fd7fd9f1263b97fa6151c4
Jorge Vera <112434926+jorgeaveraf@users.noreply.github.com>
2023-09-08 09:47:44 -0600

    Work (#8)
    * consumir api
    
    * Header (#3) (#4)
    
    * Primer cambio en el header
    
    * create services
    
    * Actualizar header
    
    * retoques en work

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { WorkService } from '../Services/work.service';
 
-
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
@@ -35,5 +34,32 @@ export class WorkExperienceComponent {
     );
   }
 
-}
+  company: string = "SIP";
+  Sdate: string = "jul 2021";
+  Edate: string = "currently";
+  role: string = "data analyst";
+  city: string = "Orizaba";
+  country: string = "Mexico";
+  accomplishments: string = "some text";
 
+
+
+  workExperience: any[] = [];
+
+  constructor(private workService: WorkService) { }
+
+  ngOnInit(): void {
+    this.workService.getHeader().subscribe((data: any) => {
+      this.workExperience = data.map((item: any) => {
+        return {
+          Sdate: `${item.fecha_inicio} - ${item.fecha_fin}`,
+          city: `${item.ciudad}, ${item.pais}`,
+          role: item.puesto,
+          company: item.empresa,
+          accomplishments: item.logros
+        };
+      });
+    });
+  }
+}
+}
