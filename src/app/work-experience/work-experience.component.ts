9514f79c5775791ecfad5fff4014294cf69bbe30
Jorge Vera <112434926+jorgeaveraf@users.noreply.github.com>
2023-09-12 12:50:27 -0600

    Update work-experience.component.ts

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,32 +7,7 @@ import { WorkService } from '../Services/work.service';
   styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent {
-  company: string = "SIP";
-  Sdate: string = "jul 2021";
-  Edate: string = "currently";
-  role: string = "data analyst";
-  city: string = "Orizaba";
-  country: string = "Mexico";
-  accomplishments: string = "some text";
-  
 
-  constructor(private workService: WorkService) { }
-
-  ngOnInit(): void {
-    this.workService.getHeader().subscribe((data: any) => {
-      console.log(data);
-
-      this.role = data.puesto;
-      this.company = data.empresa;
-      this.Sdate = data.fecha_inicio;
-      this.Edate = data.fecha_fin;
-      this.city = data.ciudad;
-      this.country = data.pais;
-      this.accomplishments = data.logros;
-      
-    }
-    );
-  }
 
   company: string = "SIP";
   Sdate: string = "jul 2021";
@@ -61,5 +36,5 @@ export class WorkExperienceComponent {
       });
     });
   }
-}
+
 }
